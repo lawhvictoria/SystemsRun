23327f1e5abca80f998f83b1e13c54fd323c818e
Victoria Law <vhlaw@calpoly.edu>
2018-05-06 22:19:11 -0700

    MenuWorkish

--- 309-SystemsRun/src/main/java/control/App.java
+++ 309-SystemsRun/src/main/java/control/App.java
@@ -25,6 +25,9 @@ public class App extends PApplet {
     final int PAUSE = 3;
     boolean click = false;
     PImage img;
+    PImage img2;
+    PImage img3;
+    PImage dimg;
 
     // Private Instance
     private World world; // TODO: Move this elsewhere
@@ -43,7 +46,10 @@ public class App extends PApplet {
 
     @Override
     public void setup() {
+    		dimg = loadImage("src/images/playBackground.gif");
     		img = loadImage("src/images/mainScreen.png");
+    		img2 = loadImage("src/images/characterSettings.png");
+    		img3 = loadImage("src/images/highscores.png");
     		
         surface.setTitle("Systems Run");
 
@@ -78,14 +84,53 @@ public class App extends PApplet {
 
     @Override
     public void draw() {
+    		fill(204, 102, 0);
+    		rect(425, 125, 450, 300);
     		if (state == 0) {
     			image(img, 0, 0);
+    			fill(204, 102, 0);
+        		//rect(352, 408, 272, 100);
+        		//rect(398, 525, 184, 68);
+        		//rect(398, 603, 184,68);
+        		//rect(23, 610, 173, 68);
+    			//ellipse(946, 646, 80, 80);
     		}
+    		
+    		if(state == 1) {
+    			//image(dimg, 0, 0);
+    			background(200, 200, 200);
+    	        world.update();
+    		}
+    		
+    		if(state == 2) {
+    			image(img2, 0, 0);
+    		}
+    		
+    		if(state == 3) {
+    			image(img3, 0, 0);
+    		}
+    		
     }
     
     public void mousePressed() {
-    		if (mouseX < 425 & mouseX > 125 & mouseY <450 & mouseY >300 ) {
+    		if (mouseX < 624 & mouseX > 352 & mouseY < 508 & mouseY > 408 ) {
     			click = true;
+    			state = 1;
+    		}
+    		
+    		if (mouseX < 582 & mouseX > 398 & mouseY < 593 & mouseY > 525 ) {
+    			click = true;
+    			state = 2;
+    		}
+    		
+    		if (mouseX < 582 & mouseX > 398 & mouseY < 671 & mouseY > 603 ) {
+    			click = true;
+    			state = 3;
+    		}
+    		
+    		if (mouseX < 1026 & mouseX > 946 & mouseY < 726 & mouseY > 646 ) {
+    			click = true;
+    			state = 0;
     		}
     	}
 }
