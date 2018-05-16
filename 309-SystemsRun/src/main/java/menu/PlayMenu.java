d97cde2712cd3041e1ac0312b8d2a414920e00c1
Victoria Law <vhlaw@calpoly.edu>
2018-05-15 23:31:30 -0700

    changes

---
+++ 309-SystemsRun/src/main/java/menu/PlayMenu.java
@@ -0,0 +1,15 @@
+package menu;
+
+import control.App;
+import control.World;
+
+public class PlayMenu extends Menu{
+	public PlayMenu(App app) {
+        super(app, "src/images/PlayOverlay.png");
+    }
+    
+    @Override
+    public void display() {
+        // create buttons in right spots and add to elements array
+    }
+}
